9960c21ef26d4770405491ec60ce0bcc19019887
Zanini <everton.zanini@signainfo.com.br>
2022-09-22 08:34:21 -0300

    feat: instalado/configurado sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialetic: 'postgres',
+  host: 'localhost',
+  username: 'postegres',
+  password: 'admin',
+  database: 'tasklist',
+  define: {
+    timestamp: true,
+    underscore: true,
+    underscoreAll: true,
+  }
+};
